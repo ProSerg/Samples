2dccd16e380dec8509bc16e9deeb18985c5d3cee
smarkin <markin@gollard.ru>
2016-03-30 18:58:46 +0300

    Test commit from Eclipse project with Ant builder.

---
+++ TestAntEclipse/src/ru/home/HelloWorld.java
@@ -0,0 +1,7 @@
+package ru.home;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println(new Text("Hello,World!!!"));
+	}
+}
---
+++ TestAntEclipse/src/ru/home/Text.java
@@ -0,0 +1,14 @@
+package ru.home;
+
+public class Text {
+	private String text;
+	
+	public Text(String text) {
+		this.text=text;
+	}
+	
+	@Override
+	public String toString() {
+		return text;
+	}
+}
