3458101e0e9930ef6e22f3d4cec3d6295b27dcc2
smarkin <markin@gollard.ru>
2016-04-08 19:10:47 +0300

    update [+] add build glew [+] add build glog [+] add build freeimage [+] add build live55 [*] update libxml2 [*] update build.sh

--- example_1/tree_cmake/build.sh
+++ example_1/tree_cmake/build.sh
@@ -9,19 +9,60 @@ target=source
 
 cd $target
 
-for dir in $dirs; 
-do
-  echo "Build: $dir"
-  mkdir -p ./$dir
-done
-
-cd ./libxml2
-
-cp -r ./../../CMakes/libxml2/CMakeLists.txt ./
+name=libxml2
+echo "Build: $name"
+mkdir -p ./$name
+cd ./$name
+cp -r ./../../CMakes/$name/CMakeLists.txt ./
 if ! [ -d src ]; then
-	wget ftp://xmlsoft.org/libxml2/libxml2-2.9.3.tar.gz -O - | tar -xz 
-	mv libxml2-2.9.3 src
+	git clone https://git.gnome.org/browse/libxml2 ./src
 fi 
- 
+cd - 
 
+name=glew
+echo "Build: $name"
+mkdir -p ./$name
+cd ./$name
+cp -r ./../../CMakes/$name/CMakeLists.txt ./
+if ! [ -d src ]; then
+	git clone https://github.com/nigels-com/glew.git ./src
+fi
+cd -
+
+name=glog
+echo "Build: $name"
+mkdir -p ./$name
+cd ./$name
+cp -r ./../../CMakes/$name/CMakeLists.txt ./
+if ! [ -d src ]; then
+	git clone https://github.com/google/glog.git ./src
+fi
+cd -
+
+
+name=freeimage
+echo "Build: $name"
+mkdir -p ./$name
+cd ./$name
+cp -r ./../../CMakes/$name/CMakeLists.txt ./
+if ! [ -d src ]; then
+	wget http://downloads.sourceforge.net/freeimage/FreeImage3170.zip
+	unzip ./FreeImage3170.zip
+	mv ./FreeImage ./src
+	rm -rf FreeImage3170.zip 
+fi
+cd -
+
+name=live555
+echo "Build: $name"
+mkdir -p ./$name
+cd ./$name
+cp -r ./../../CMakes/$name/CMakeLists.txt ./
+if ! [ -d src ]; then
+	wget http://www.live555.com/liveMedia/public/live555-latest.tar.gz	
+	tar -zxf live555-latest.tar.gz
+	mv ./live ./src
+	rm -rf live555-latest.tar.gz
+fi
+cd -
 
