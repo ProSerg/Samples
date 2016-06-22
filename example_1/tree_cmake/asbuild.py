8565951b8fa977758bc9965a7ef525c6bb80e942
ProSerg <ssmarkin@yandex.ru>
2016-06-22 19:28:24 +0300

    add make & install action

--- example_1/tree_cmake/asbuild.py
+++ example_1/tree_cmake/asbuild.py
@@ -5,6 +5,7 @@ import sys
 import re
 import subprocess
 import copy
+import os 
 ####
 # OTHER MODE
 ####
@@ -15,7 +16,8 @@ import copy
 
 ####
 __arraylibs=[]
-
+__pwd=""
+_exclude=[]
 ####
 
 def usage():
@@ -24,7 +26,7 @@ def usage():
 def HandlerArgs():
 	parser = argparse.ArgumentParser(description="A prog for assembly libs")
 	parser.add_argument("--libs" ,nargs="+", help="print value", default="all")
-	parser.add_argument("--download" ,action='store_true', help="download loibs")
+	parser.add_argument("--download" ,action='store_true', help="download libs")
 	parser.add_argument("--install" ,action='store_true', help="install libs")
 	parser.add_argument("--make" ,action='store_true', help="build libs")
 	parser.add_argument("--exception" ,nargs="+", help="exception libs")
@@ -41,28 +43,56 @@ def PrintEcho(options):
 	
 # it needs for exseption "ALL" if there is another lib. 	
 def parsLibs(nameFile,array):
-	dfile = open(nameFile)
-	for line in iter(dfile):
-		p=re.compile('\s*function\s+download_([\w-]+)')
-		match=p.match(line)
-		if match:
-			array.append(match.group(1))
-	dfile.close()
+	try:
+		dfile = open(nameFile)
+	except IOError as e:
+		print("It can not find the file : {}".format(nameFile) )
+		return -1
+	else:
+		with dfile:
+			for line in iter(dfile):
+				p=re.compile('\s*function\s+download_([\w-]+)')
+				match=p.match(line)
+				if match:
+					array.append(match.group(1))
+			dfile.close()
 	return 0
 
 def Download(libs):
 	print ("DOWNLOAD: {}".format(libs))
 	#print("LIBS:{}".format(libs))
-	#for lib in iter(libs):
-		#subprocess.call("./download.sh {}".format(lib), shell=True)
-	#return res
+	for lib in iter(libs):
+		subprocess.call("./download.sh {}".format(lib), shell=True)
 	
 def Install(libs):
 	print ("INSTALL: {}".format(libs))
-
-def Make(libs):
-	print ("MAKE: {}".format(libs))
-
+	__pwd=os.path.dirname(os.path.abspath( __file__ ))
+	os.chdir("{}/CBin".format(__pwd))
+	try:
+		subprocess.call("make install", shell=True)
+	except NameError as ex:
+		print("error: can't call 'make install' ")
+		return -1
+	else:
+		os.chdir("{}".format(__pwd))
+		return 0
+		 
+def Make(excludes):
+	print ("MAKE: {}".format(excludes))
+	__pwd=os.path.dirname(os.path.abspath( __file__ ))
+	os.chdir("{}/CBin".format(__pwd))
+	try:
+		subprocess.call("rm -rf \"./*\"", shell=True)
+		subprocess.call("cmake {} ..".format(excludes), shell=True)
+		subprocess.call("make".format(excludes), shell=True)
+	except NameError as ex:
+		print("error: can't call {}".format(ex.args))
+		return -1
+	else:
+		os.chdir("{}".format(__pwd))
+		return 0
+	
+	
 def exceptlibs(libs,exps):			
 	array=[]
 	if libs == "all":
@@ -75,9 +105,25 @@ def exceptlibs(libs,exps):
 						break
 	else:
 		array=copy.copy(libs)
+		
 	return array
 	
-
+def excludelibs(libs,exps):
+	exclude=""
+	array=[]
+	if libs == "all":
+		if exps:
+			for exlib in iter(exps):
+				exclude=" ".join("-DEXCLUDE_{}:BOOL=TRUE".format(exlib))
+	else:
+		array=copy.copy(__arraylibs)
+		for lib in iter(libs):
+			for element in iter(array):
+				if lib == element:
+					array.remove(lib)
+		exclude="".join(" -DEXCLUDE_%s:BOOL=TRUE" % (stroka) for stroka in array )
+	return exclude
+	
 def HandlerOpts(options):
 	array=[]
 	libs=options.libs
@@ -85,30 +131,38 @@ def HandlerOpts(options):
 	isMake=options.make
 	isInstall=options.install
 	exps=options.exception
-	
 	array=exceptlibs(libs,exps)
+	exclude=excludelibs(libs,exps)
 	
 	if isDownload == bool(1):
 		Download(array)
 	if isMake == bool(1):
-		Make(array)
+		Make(exclude)
 	if isInstall == bool(1):
 		Install(array)
 		
 	return 0
 	
 def init():
-	parsLibs("./download.sh",__arraylibs)
+	__pwd=os.path.dirname(os.path.abspath( __file__ ))
+	
+##
+## add checking work directories
+##
+	print("PWD: {}".format(__pwd) )
+	file_download="{}/download.sh".format(__pwd)
+	res = parsLibs(file_download,__arraylibs)
+	if res == -1:
+		print("error: init")
+		return -1
 	
 
 if __name__ == "__main__":
 	print ("Hello,World!")
 	for param in sys.argv:
 		print(param)
-	init()
-	
-#	print("Arr:{}".format(array))
-	
+	if init() == -1 :
+		exit
 	options=HandlerArgs()
 	PrintEcho(options)
 	
