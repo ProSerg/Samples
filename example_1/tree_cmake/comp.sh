79c8126a4ff8911f78f60671989a767515517ae8
ProSerg <ssmarkin@yandex.ru>
2016-06-24 11:11:57 +0300

    add autocomplite for '--' options

--- example_1/tree_cmake/comp.sh
+++ example_1/tree_cmake/comp.sh
@@ -1,16 +1,29 @@
 
-_command(){ 
+_command() { 
+	flag=""
     COMPREPLY=() 
     cur="${COMP_WORDS[COMP_CWORD]}" 
     prev="${COMP_WORDS[COMP_CWORD-1]}" 
-    #subcommands_1="libxml2 glew gflags glog freeimage live555 openssl portaudio sdl mongocxx boost cpp-netlib googletest protobufogre freetype yasm ffmpeg x264 x265 rtmp libass theora vorbis fribidi Qt qtpropertybrowser qwt"
-	subcommands_1="libxml2 glew gflags glog freeimage live555 openssl portaudio sdl"
- 
-
-   if [[ ${COMP_CWORD} == 1 ]] ; then 
-        COMPREPLY=( $(compgen -W "${subcommands_1}" -- ${cur}) ) 
-        return 0
+    subcommands="libxml2 glew gflags glog freeimage live555 openssl portaudio sdl mongocxx boost cpp-netlib googletest protobufogre freetype yasm ffmpeg x264 x265 rtmp libass theora vorbis fribidi Qt qtpropertybrowser qwt --"
+	opts="--download --install --make --help"
+   
+   if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+   else   
+        for el in ${COMP_WORDS[@]}; do
+			if [[ ${el} == -* ]]; then
+				flag="scip"
+				break;
+			fi
+		done
+		
+		if [[ -z ${flag} ]]; then
+			COMPREPLY=( $(compgen -W "${subcommands}" -- ${cur}) ) 
+		fi
    fi
+   
+   return 0
+
 }
 
 complete -F _command ./asbuild.py
